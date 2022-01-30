84e6ff0b9bf8f73e981b9612ac778c7d83e01542
Caleb-Komen <calebkipyatich@gmail.com>
2022-01-30 22:31:36 +0300

    Add colours for task labels

--- app/src/main/java/com/taskmanager/taskmanagement/domain/util/Label.kt
+++ app/src/main/java/com/taskmanager/taskmanagement/domain/util/Label.kt
@@ -1,9 +1,12 @@
 package com.taskmanager.taskmanagement.domain.util
 
-enum class Label {
-    NONE,
-    FEATURE,
-    BUG,
-    IMPORTANT,
-    IDEA
+import androidx.annotation.ColorRes
+import com.taskmanager.taskmanagement.R
+
+enum class Label(@ColorRes colour: Int) {
+    NONE(R.color.white),
+    FEATURE(R.color.green),
+    BUG(R.color.red),
+    IMPORTANT(R.color.blue),
+    IDEA(R.color.yellow)
 }
