cbb8aac17e9602605efa474c297616adccb0749a
Caleb-Komen <calebkipyatich@gmail.com>
2022-03-03 15:02:27 +0300

    The dashboard UI design. The dashboard shows a bit of statistics like number of projects, the tasks assigned to current user and scheduled tasks for the current user.

--- app/src/main/java/com/taskmanager/taskmanagement/domain/usecases/GetUserUseCase.kt
+++ app/src/main/java/com/taskmanager/taskmanagement/domain/usecases/GetUserUseCase.kt
@@ -1,12 +1,13 @@
 package com.taskmanager.taskmanagement.domain.usecases
 
+import com.taskmanager.taskmanagement.domain.model.User
 import com.taskmanager.taskmanagement.domain.repository.UserRepository
 import javax.inject.Inject
 
 class GetUserUseCase @Inject constructor(
     private val userRepository: UserRepository
 ) {
-    suspend operator fun invoke(id: String){
-        userRepository.getUserById(id)
+    suspend operator fun invoke(id: String): User?{
+        return userRepository.getUserById(id)
     }
 }
