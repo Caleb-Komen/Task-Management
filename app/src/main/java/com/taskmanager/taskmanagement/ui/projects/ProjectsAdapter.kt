b6c5660f37f9dd7f9d7a0fc8a0ad46d6a807bee8
Caleb-Komen <calebkipyatich@gmail.com>
2022-03-10 14:50:16 +0300

    Multi-view recyclerview. The recyclerview contains list of projects together with a button for creating new project.

--- app/src/main/java/com/taskmanager/taskmanagement/ui/projects/ProjectsAdapter.kt
+++ app/src/main/java/com/taskmanager/taskmanagement/ui/projects/ProjectsAdapter.kt
@@ -1,28 +1,41 @@
 package com.taskmanager.taskmanagement.ui.projects
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.taskmanager.taskmanagement.R
+import com.taskmanager.taskmanagement.databinding.ListItemNewProjectBinding
 import com.taskmanager.taskmanagement.databinding.ListItemProjectBinding
 import com.taskmanager.taskmanagement.domain.model.Project
 
-class ProjectsAdapter: ListAdapter<Project, ProjectsViewHolder>(DIFF_UTIL) {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProjectsViewHolder {
-        return ProjectsViewHolder(
-            ListItemProjectBinding.inflate(
-                LayoutInflater.from(parent.context),
-                parent,
-                false
-            )
-        )
+class ProjectsAdapter: ListAdapter<Project, RecyclerView.ViewHolder>(DIFF_UTIL) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return if (viewType == R.layout.list_item_project){
+            ProjectsViewHolder.create(parent)
+        } else {
+            NewProjectItemViewHolder.create(parent)
+        }
     }
 
-    override fun onBindViewHolder(holder: ProjectsViewHolder, position: Int) {
-        val project = getItem(position)
-        holder.bind(project)
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (getItemViewType(position) == R.layout.list_item_project) {
+            val project = getItem(position)
+            (holder as ProjectsViewHolder).bind(project)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return currentList.size + 1
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (position == currentList.size || currentList.isNullOrEmpty()){
+            R.layout.list_item_new_project
+        } else {
+            R.layout.list_item_project
+        }
     }
 }
 
@@ -33,6 +46,32 @@ class ProjectsViewHolder(private val binding: ListItemProjectBinding): RecyclerV
                 projectName = project.name
                 executePendingBindings()
             }
+        }
+    }
+
+    companion object{
+        fun create(parent: ViewGroup): ProjectsViewHolder{
+            return ProjectsViewHolder(
+                ListItemProjectBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+            )
+        }
+    }
+}
+
+class NewProjectItemViewHolder(private val binding: ListItemNewProjectBinding): RecyclerView.ViewHolder(binding.root){
+    companion object{
+        fun create(parent: ViewGroup): NewProjectItemViewHolder{
+            return NewProjectItemViewHolder(
+                ListItemNewProjectBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+            )
         }
     }
 }
